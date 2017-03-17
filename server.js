419151411ca655b3bcbfbefeabe2ff68e503a4d1
Rochelle Valdez <rochellevee@gmail.com>
2017-03-16 17:25:22 -0700

    DB connected

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var mongo = require('mongo');
 var mongoose = require('mongoose');
 var app = express();
 
+
+/*CONNECT TO DATABASE*/
+//make sure to uncomment which connection you're using
+
+// var connection = 'mongodb://thyme:thyme@ds133340.mlab.com:33340/orion-thyme';
+var connection = 'mongodb://localhost/thyme';
+mongoose.connect(connection);
+mongoose.connection.once('open', function() {
+  console.log('Thyme after thyme on: ' + connection)
+})
+
+
+
 /*CONNECT TO SERVER*/
 var port = process.env.PORT || 3000;
 
