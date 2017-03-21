a869725bcf9d34a8bf27e7bd96b661c521953a2e
Ariel <Ariel.Salem1989@gmail.com>
2017-03-20 17:14:02 -0700

    added cooking page - work in progress

--- client/oath.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-  facebook: {
-    clientID: '181495925690983',
-    clientSecret: 'e04cf1e4c92e1d600a1420e6a05e0184',
-    callbackUrl: 'https://orion-thyme.herokuapp.com/auth/facebook/callback'
-  }
-};
-
-module.exports = ids;
