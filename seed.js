e3672eada59c476a8d91e61ff941b287ccf774b5
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-22 12:17:44 -0500

    removed seed and data files.
    removed seed and data files.

--- seed.js
+++
@@ -1,8 +0,0 @@
-
-let exec = require('child_process').exec
-let command = 'mongoimport --db orion-thyme --collection recipes --drop --file data.json --jsonArray'
-
-exec(command, function(error, stdout, stderr){
-
-});
-
