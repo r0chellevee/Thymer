8e2eb518b858ae15b76350dca6815fe116ac833e
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-22 10:27:45 -0500

    updated seeder
    updated seeder

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 
 let exec = require('child_process').exec
-let command = 'mongoimport --db DATABASENAME --collection COLLECTIONNAME --drop --file data.json --jsonArray'
+let command = 'mongoimport --db orion-thyme --collection recipes --drop --file data.json --jsonArray'
 
 exec(command, function(error, stdout, stderr){
 
