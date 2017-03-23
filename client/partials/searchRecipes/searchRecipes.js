8162a0c7ac6ed2f5880d3ad79e72195ec3bcafa0
Rochelle Valdez <rochellevee@gmail.com>
2017-03-23 10:51:53 -0700

    deleted unnecessary dependencies

--- client/partials/searchRecipes/searchRecipes.js
+++ client/partials/searchRecipes/searchRecipes.js
@@ -2,7 +2,7 @@ angular.module('thymer.searchRecipes', [])
 
 .controller('searchRecipesController', function($scope, $location, Recipes) {
 
-  console.log('getrecipes:', Recipes.getRecipes);
+
   Recipes.getRecipes()
   .then(function(data) {
     $scope.recipes = data;
